f2739b6743e4d1cf207efa3927d75922ae53e77f
KazooTTT <wangyijin1999@qq.com>
2022-10-19 20:00:48 +0800

    feat: add link and router navigate to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>hello ssr</h1>
+      <div>
+        <a href="http://127.0.0.1:3000/demo">链接跳转</a>
+      </div>
       <button
         onClick={(): void => {
-          alert("hello-ssr");
+          navigate("/demo");
         }}
       >
-        click button
+        路由跳转
       </button>
     </div>
   );
