443518a92dc69605b1c530dc45fbaea049bbea19
KazooTTT <wangyijin1999@qq.com>
2022-10-21 18:05:45 +0800

    feat: 新增html title, 新增跳转

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,47 @@
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>hello ssr</h1>
+    <>
+      <Helmet>
+        <title>简易的服务器端渲染 - HOME</title>
+        <meta name="description" content="服务器端渲染"></meta>
+      </Helmet>
       <div>
-        <a href="http://127.0.0.1:3000/demo">链接跳转</a>
+        <h1>hello ssr</h1>
+        <div>
+          <a href="http://127.0.0.1:3000/demo">链接跳转</a>
+        </div>
+        <button
+          onClick={(): void => {
+            navigate("/demo");
+          }}
+        >
+          路由跳转
+        </button>
       </div>
-      <button
-        onClick={(): void => {
-          navigate("/demo");
-        }}
-      >
-        路由跳转
-      </button>
-    </div>
+      <div>
+        <h1>hello-ssr</h1>
+        <button
+          onClick={(): void => {
+            alert("hello-ssr");
+          }}
+        >
+          alert
+        </button>
+        <a href="http://127.0.0.1:3000/demo">链接跳转</a>
+        <span
+          onClick={(): void => {
+            navigate("/demo");
+          }}
+        >
+          路由跳转
+        </span>
+      </div>
+    </>
   );
 };
 
