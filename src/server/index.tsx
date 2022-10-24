edacc5b34be8e172284f606489481a24160507ad
KazooTTT <wangyijin1999@qq.com>
2022-10-24 19:29:42 +0800

    feat: import body-parser

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: KazooTTT wangyijin1999@qq.com
+ * @Date: 2022-10-18 00:34:45
+ * @LastEditors: KazooTTT wangyijin1999@qq.com
+ * @LastEditTime: 2022-10-24 19:28:13
+ * @FilePath: /ssr-server-study/src/server/index.tsx
+ * @Description:
+ */
 import express from "express";
 import childProcess from "child_process";
 import { renderToString } from "react-dom/server";
@@ -5,10 +13,26 @@ import path from "path";
 import router from "@/router";
 import { Route, Routes } from "react-router-dom";
 import { StaticRouter } from "react-router-dom/server";
+import { Helmet } from "react-helmet";
 
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use(express.static(path.resolve(process.cwd(), "client_build")));
+// 静态资源路径
+const staticPath = path.join(__dirname, "client_build");
+app.use(express.static(staticPath));
+
+// 解析body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 启一个post服务
+app.post("/api/getDemoData", (req, res) => {
+  res.send({
+    data: req.body,
+    status_code: 0,
+  });
+});
 
 app.get("*", (req, res) => {
   const content = renderToString(
@@ -21,14 +45,20 @@ app.get("*", (req, res) => {
     </StaticRouter>
   );
 
+  const helmet = Helmet.renderStatic();
+
   res.send(`
-    <html
-      <body>
-        <div id="root">${content}</div>
-        <script src="/index.js"></script>
-      </body>
-    </html>
-  `);
+      <html
+        <head>
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+        </head>
+        <body>
+          <div id="root">${content}</div>
+          <script src="/index.js"></script>
+        </body>
+      </html>
+    `);
 });
 
 app.listen(3000, () => {
